dfd5c669e254678c6b9ea347ac7090633b672433
Yashwanth reddy <yash868686reddy@gmail.com>
2022-02-11 17:56:40 -0500

    Done TailwindCSS with Create-React-App Front-End setup

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
