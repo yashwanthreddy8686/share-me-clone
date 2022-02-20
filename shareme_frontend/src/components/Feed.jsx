3c41dfda731ce21e81c6b72465dcbc9aaf7441ea
Yashwanth reddy <yash868686reddy@gmail.com>
2022-02-19 20:38:30 -0500

    Initial Feed Setup

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client} from '../client.js'
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 function Feed() {
+
+  const [loading,setLoading] = useState(false);
+  const [pins,setPins] = useState(null);
+
+  const {categoryId} = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    
+    if(categoryId){
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      })
+    } else{
+
+    }
+  },[categoryId])
+
+  if(loading){
+    return <Spinner message= {`we are adding new ideas to your feed`} />
+  }
   return (
     <div>Feed</div>
   )
 }
 
-export default Feed
+export default Feed;
---
+++ shareme_frontend/src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BallTriangle} from 'react-loader-spinner';
+
+function Spinner({message}) {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-full">
+     <BallTriangle color="#00BFFF" height={80} width={80} />
+
+      <p className="text-lg text-center px-2">{message}</p>
+    </div>
+  )
+}
+
+export default Spinner;
