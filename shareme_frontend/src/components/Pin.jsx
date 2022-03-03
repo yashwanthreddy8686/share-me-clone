9010d6413dcde749c5ca2d86220900b64dcc5852
Yashwanth reddy <yash868686reddy@gmail.com>
2022-03-02 23:43:09 -0500

    Done With Save Pin

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -5,12 +5,18 @@ import { client, urlFor } from '../client';
 import { MdDownloadForOffline } from 'react-icons/md'
 import { AiTwotoneDelete } from 'react-icons/ai'
 import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+import { fetchUser } from '../utils/fetchUser';
 
-function Pin({ pin: { postedBy, image, _id, destination } }) {
+function Pin({ pin: { postedBy, image, _id, destination, save } }) {
   const [postHovered, setPostHovered] = useState(false);
   const [savingPost, setSavingPost] = useState(false);
 
   const navigate = useNavigate();
+  const user = fetchUser();
+
+  const alreadySaved = !!(save?.filter((item) => item.postedBy._id === user.googleId))?.length;
+
+  
 
   return (
     <div className='m-2'>
@@ -18,15 +24,15 @@ function Pin({ pin: { postedBy, image, _id, destination } }) {
         className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out'
         onMouseEnter={() => setPostHovered(true)}
         onMouseLeave={() => setPostHovered(false)}
-        onClick={() => Navigate(`/pin-detail/${_id}`)}
+        onClick={() => navigate(`/pin-detail/${_id}`)}
       >
         <img className='rounded-lg w-full' alt='user-post' src={urlFor(image).width(250).url()} />
         {postHovered && (
-          <div 
-          className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
-          style={{height: '100%'}}
+          <div
+            className='absolute top-0 w-full h-full flex flex-col justify-between p-1 pr-2 pt-2 pb-2 z-50'
+            style={{ height: '100%' }}
           >
-            <div className='flex items-center justify-center'>
+            <div className='flex items-center justify-between'>
               <div className='flex gap-2'>
                 <a
                   href={`${image?.asset?.url}?dl=`}
@@ -34,9 +40,23 @@ function Pin({ pin: { postedBy, image, _id, destination } }) {
                   onClick={(e) => e.stopPropagation()}
                   className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
                 >
-                  <MdDownloadForOffline/>
+                  <MdDownloadForOffline />
                 </a>
               </div>
+              {alreadySaved ? (
+                <button type='button' 
+                className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">
+                   {save?.length} Saved
+                  </button>
+              ) : (
+                <button 
+                onClick={(e) => {
+                  e.stopPropagation();
+                  savePin(_id);
+                }}
+                type='button'
+                  className="bg-red-500 opacity-70 hover:opacity-100 text-white font-bold px-5 py-1 text-base rounded-3xl hover:shadow-md outline-none">Save</button>
+              )}
             </div>
           </div>
         )}
